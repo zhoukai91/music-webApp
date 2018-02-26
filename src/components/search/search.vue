101bf267fa0fd408dac961c52af804e6249b55c8
victor_zk <zhoukai8887@qq.com>
2018-02-26 18:21:13 +0800

    搭建项目结构

---
+++ src/components/search/search.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  搜索
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'search'
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/components/singer/singer.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  歌手
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'singer'
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
